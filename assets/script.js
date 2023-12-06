ea0c9aa683e3aa1bd9e3f331f596cb2fc4bc71d5
peggyp <proffit.peggy@yahoo.fr>
2023-12-06 18:31:38 +0100

    add bullets points

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+//Variables du projet//
+
 const arrowLeft = document.querySelector(".arrow_left")
 	arrowLeft.addEventListener("click",()=>{
 		console.log("clic gauche")
@@ -10,20 +12,33 @@ const arrowRight = document.querySelector(".arrow_right")
 
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 console.log(slides)
+
+const dots=document.querySelector(".dots")
+let index=0
+
+//Ajout des bullets points//
+function displayDots() {
+	for (let i=0; i<slides.length; i++){
+	const dot = document.createElement ("div")
+		dot.classList.add("dot")
+		dots.appendChild(dot)
+	}
+}
+displayDots()
